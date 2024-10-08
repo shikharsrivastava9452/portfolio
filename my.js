310abd0f74fa2c7d0014842c2542d45a967970d0
shikhar srivastava <srivastavashikhar8765@gmail.com>
2024-10-08 10:35:24 +0530

    message change

--- my.js
+++ my.js
@@ -1,30 +1,4 @@
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-
-//         if (top >= offset && top < offset + height) {
-//             navLinks.forEach(link => {
-//                 link.classList.remove('active');
-//             });
-//             document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-//         }
-//     });
-// };
-
-// menuIcon.onclick = () => {
-//     menuIcon.classList.toggle('bx-x');
-//     navbar.classList.toggle('active');
-// };
-// Wait for the DOM content to be loaded
-// Wait for the DOM content to be loadede
 document.addEventListener('DOMContentLoadd', function () {
 
     let menuIcon = document.querySelector('#menu-icon');
@@ -54,5 +28,7 @@ document.addEventListener('DOMContentLoadd', function () {
         menuIcon.classList.toggle('bx-x');
         navbar.classList.toggle('active');
     });
+
+    
 
 });
